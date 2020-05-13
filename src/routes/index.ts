c482cd3fa8f534966e27796f7054d58ced7369cf
Eduardo Faria <z.eduardofaria@gmail.com>
2020-05-13 16:24:24 -0300

    User routes, creation, credentials validation

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,12 @@
 import { Router } from 'express';
+
 import appointmentsRouter from './appointments';
+import usersRouter from './users';
+import sessionsRouter from './sessions';
 
 const routes = Router();
 
 routes.use('/appointments', appointmentsRouter);
+routes.use('/sessions', sessionsRouter);
 
 export default routes;
---
+++ src/routes/sessions.ts
@@ -0,0 +1,24 @@
+import { Router } from 'express';
+
+import AuthenticateUserService from '../services/AuthenticateUserService';
+
+const sessionsRouter = Router();
+
+sessionsRouter.post('/', async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const authenticateUser = new AuthenticateUserService();
+
+    const { user } = await authenticateUser.execute({
+      email,
+      password,
+    });
+
+    return res.json({ user });
+  } catch (err) {
+    return res.status(400).json({ err: err.message });
+  }
+});
+
+export default sessionsRouter;
