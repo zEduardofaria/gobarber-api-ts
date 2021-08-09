c1062f92f58c449de8aa48382cacd75d0133205f
Eduardo Faria <z.eduardofaria@gmail.com>
2021-08-09 10:40:06 -0300

    Adding inspect to ts node, using transpile-only instead of transpileOnly

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,10 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
-app.get('/get', (request, response) => {
-  return response.json({ message: 'Hello You' });
-});
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333, () => {
   console.log('🚀 Server started on port 3333!');
